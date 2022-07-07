b4d7349bcfa6a2ee1d53a775b08a986d0947467f
LucWasTaken <84717490+LucWasTaken@users.noreply.github.com>
2022-07-07 21:52:15 +0200

    1

---
+++ dn-loadingscreen/assets/scripts/config.js
@@ -0,0 +1,29 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "5.182.206.102:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Tilslut vores community!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/8frBK7J8ES"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Magnus2109", description: "Projektleder", color: "#fff", image: "https://media.discordapp.net/attachments/945393798983336019/951961822615973948/new-southland-roleplay-serious-roleplay-custom-vehicles-peds-label-text-symbol-logo-transparent-png-2125237.png"},
+    {name: "Kasper", description: "Projektleder", color: "#fff", image: "https://media.discordapp.net/attachments/945393798983336019/951961822615973948/new-southland-roleplay-serious-roleplay-custom-vehicles-peds-label-text-symbol-logo-transparent-png-2125237.png"},
+    {name: "Oliver Holm", description: "Community Manager", color: "#0195FD", image: "https://media.discordapp.net/attachments/945393798983336019/951961822615973948/new-southland-roleplay-serious-roleplay-custom-vehicles-peds-label-text-symbol-logo-transparent-png-2125237.png"},
+    {name: "LucWasTaken", description: "Udviklings ansvarlig", color: "#0195FD", image: "https://media.discordapp.net/attachments/945393798983336019/951961822615973948/new-southland-roleplay-serious-roleplay-custom-vehicles-peds-label-text-symbol-logo-transparent-png-2125237.png"},
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Vores Discord", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
